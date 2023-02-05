3828047c08ba95b1a4fc03a892f8acc377de5445
haruo75 <105282664+haruo75@users.noreply.github.com>
2023-02-05 09:35:04 +0800

    Update update.py

--- update.py
+++ update.py
@@ -3,6 +3,7 @@ from os import path as ospath, environ, remove as osremove
 from subprocess import run as srun, call as scall
 from pkg_resources import working_set
 from requests import get as rget
+from dotenv import load_dotenv
 from pymongo import MongoClient
 
 if ospath.exists('log.txt'):
@@ -12,6 +13,8 @@ if ospath.exists('log.txt'):
 basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     handlers=[FileHandler('log.txt'), StreamHandler()],
                     level=INFO)
+
+load_dotenv('config.env', override=True)
 
 try:
     if bool(environ.get('_____REMOVE_THIS_LINE_____')):
